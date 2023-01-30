d9fa1fbeffcea8d72578a14ae0cddb9c5154cd47
Fxanz <107518652+Fxanz@users.noreply.github.com>
2023-01-30 15:11:37 +0800

    I want this website to have an anime theme, but I know my teacher will kill me if I do that, so... ☠️

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-const loginsystem = () => {
-  const user = document.getElementById("username").value;
-  const pass = document.getElementById("password").value;
+function login() {
+  var user = document.getElementById("username").value;
+  var pass = document.getElementById("password").value;
   if (user == "admin" && pass == "123") {
-    alert("Hello Admin");
-  } else if (user == "robert" && pass == "234") {
-    alert("mana ayangmu bang?");
-  } else if (user == "edson" && pass == "345") {
-    alert("masih fokus!");
-  } else if (user == "eka" && pass == "456") {
-    alert("lagi nyari cewe baru");
-  } else if (user == "prama" && pass == "567") {
-    alert("pasti lagi main game tuh!");
+    alert("berhasih masuk");
   } else {
-    alert("You Are Not Admin!");
+    alert("anda gagal masuk");
   }
-};
+}
